091d54e3a185d8b0ef4f5b0fd81238249196fc30
Sam Bowen <samhbowen@gmail.com>
2025-04-08 19:23:58 -0700

    template tags

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    # Routes will be added here
+    path('', views.home, name='home'), # Define a URL pattern for the home page
+    path('about/', views.about, name='about'), # Define a URL pattern for the about page
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
+# main_app/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>');
+
+# Define the about view function
+def about(request):
+    return render(request, 'about.html')
